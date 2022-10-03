5bc9be49cb89fb3d27044793c8a357c947d6cf7a
Axel Winter <mail@axelwinter.de>
2022-10-03 10:38:38 +0200

    Replace pexels images with unsplash ones

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import GallerySlider from './GallerySlider/GallerySlider.js';
-import Magnifier from './Magnifier/Magnifier.js';
+// import Magnifier from './Magnifier/Magnifier.js';
 
 import './App.css';
 
 function App() {
 	const galleryList = [
-		{url: 'pexels-maria-loznevaya-13828059.jpeg', altText: '13828059'},
-		{url: 'pexels-maria-loznevaya-13816113.jpeg', altText: '13816113'},
-		{url: 'pexels-maria-loznevaya-13815029.jpeg', altText: '13815029'}
+		{url: 'eugene-golovesov--eOGjaog7h0-unsplash.jpg', altText: '1'},
+		{url: 'eugene-golovesov-3o72hSvigbk-unsplash.jpg', altText: '2'},
+		{url: 'juan-carlos-frias-brito--uKRelOEcXw-unsplash.jpg', altText: '3'},
+		{url: 'mike-houser-ASOGZQvLKt0-unsplash.jpg', altText: '4'}
 	];
-	const options = {img: '1.jpg', width: 300, height: 450, zoomWidth: 500, offset: {vertical: 0, horizontal: 10}};
+	// const options = {img: 'eugene-golovesov--eOGjaog7h0-unsplash.jpg', width: 300, height: 450, zoomWidth: 500, offset: {vertical: 0, horizontal: 10}};
 
 	return (
 		<div className="container text-center">
 			<div className="row">
 				<div className="col-lg-6 col-md-8 mx-auto">
 					<GallerySlider slides={galleryList} />
-					{/*<ReactMagnifier options={options} />*/}
+					{/*<Magnifier options={options} />*/}
 				</div>
 			</div>
 		</div>
