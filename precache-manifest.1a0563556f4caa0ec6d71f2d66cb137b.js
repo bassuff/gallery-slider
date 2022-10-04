5f6c62eed865df230969badaa9e3436eb20de19b
Axel Winter <mail@axelwinter.de>
2022-10-04 12:00:24 +0200

    Updates

---
+++ precache-manifest.1a0563556f4caa0ec6d71f2d66cb137b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0bda019440630938d48e",
+    "url": "/gallery-slider/static/css/main.49e421d7.chunk.css"
+  },
+  {
+    "revision": "0bda019440630938d48e",
+    "url": "/gallery-slider/static/js/main.0bda0194.chunk.js"
+  },
+  {
+    "revision": "c972895fac6c9db70f84",
+    "url": "/gallery-slider/static/js/1.c972895f.chunk.js"
+  },
+  {
+    "revision": "261006b5618843707ed6",
+    "url": "/gallery-slider/static/js/2.261006b5.chunk.js"
+  },
+  {
+    "revision": "204d6b2d3a6c7a81c056",
+    "url": "/gallery-slider/static/js/runtime~main.204d6b2d.js"
+  },
+  {
+    "revision": "ae215bc9c1e4db00d31bb24a597ab8a7",
+    "url": "/gallery-slider/index.html"
+  }
+];
